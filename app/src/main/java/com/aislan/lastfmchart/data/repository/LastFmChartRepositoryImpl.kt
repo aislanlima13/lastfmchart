c8fb912eb5fe68faf08fd356d30701d1931196ad
aislanlima13 <aislan.martins99@gmail.com>
2024-06-07 09:20:10 -0300

    Creates an Impl to LastFmChartRepositoryImpl from domain layer

---
+++ app/src/main/java/com/aislan/lastfmchart/data/repository/LastFmChartRepositoryImpl.kt
@@ -0,0 +1,33 @@
+package com.aislan.lastfmchart.data.repository
+
+import com.aislan.lastfmchart.data.model.TopAlbums
+import com.aislan.lastfmchart.data.repository.datasource.LastfmRemoteDataSource
+import com.aislan.lastfmchart.data.util.Resource
+import com.aislan.lastfmchart.domain.repository.LastFmChartRepository
+import retrofit2.Response
+
+class LastFmChartRepositoryImpl(
+    private val lastfmRemoteDataSource: LastfmRemoteDataSource
+): LastFmChartRepository {
+    override suspend fun getUserTopAlbums(
+        user: String,
+        period: String?,
+        limit: Int?,
+        apiKey: String
+    ): Resource<TopAlbums> {
+        return responseToResource(lastfmRemoteDataSource.getTopAlbums())
+    }
+
+    private fun responseToResource(response: Response<TopAlbums>): Resource<TopAlbums> {
+        if (response.isSuccessful) {
+            response.body()?.let { result ->
+                return Resource.Success(result)
+            }
+        }
+        return Resource.Error(response.message(), null)
+    }
+
+    override fun shareTopAlbums() {
+        TODO("Not yet implemented")
+    }
+}
