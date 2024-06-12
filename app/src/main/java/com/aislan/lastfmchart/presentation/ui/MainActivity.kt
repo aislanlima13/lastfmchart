e155f4b7567f54f77362c7649f86e827285ee575
aislanlima13 <aislan.martins99@gmail.com>
2024-06-12 09:49:57 -0300

    Creates MainActivity and makes first API call and generates log

--- app/src/main/java/com/aislan/lastfmchart/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/aislan/lastfmchart/presentation/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.aislan.lastfmchart.presentation.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,11 +12,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.aislan.lastfmchart.presentation.ui.ui.theme.LastfmChartTheme
+import com.aislan.lastfmchart.presentation.viewmodel.LastFmViewModel
+import com.aislan.lastfmchart.presentation.viewmodel.LastFmViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var factory: LastFmViewModelFactory
+    lateinit var viewModel: LastFmViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initViewModel()
+        observer()
         enableEdgeToEdge()
         setContent {
             LastfmChartTheme {
@@ -26,6 +41,17 @@ class MainActivity : ComponentActivity() {
                     )
                 }
             }
+        }
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProvider(this, factory)[LastFmViewModel::class.java]
+    }
+
+    private fun observer() {
+        viewModel.getTopAlbums("AislanLima", "7day", 1)
+        viewModel.topAlbums.observe(this) {
+            Log.i("top_album", it.data?.topalbums!!.album[0].name)
         }
     }
 }
